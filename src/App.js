c5f5eca4ce542a66182d5ff9ff44858ec0ff3ba5
Nicolas Ayala <roacutanico@gmail.com>
2022-07-13 17:24:07 -0400

    Index and App modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'wouter';
+import Home from './components/Home';
+import InfoArtist from './components/InfoArtist';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-sm gradient min-h-screen">
+      <Route path='/' component={Home} />
+      <Route path="/artist/:id" component={InfoArtist} />
     </div>
   );
 }
